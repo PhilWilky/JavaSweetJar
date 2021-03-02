6fa189e55dfd8abdf3c6440c5922ee8a838f9858
Phil Wilkinson <phil@philwilky.me>
2021-03-02 16:22:27 +0000

    Added amount of guesses

--- src/com/philwilky/Main.java
+++ src/com/philwilky/Main.java
@@ -13,39 +13,48 @@ public class Main {
         // generate random integers in range 0 to 1500
         int sweetCount = rand.nextInt(1501);
 
+        // Keep a track on how many guesses the user made
+        int numGuess = 0;
+
         // getting user input
         System.out.println("Hello, what is your name: ");
 
-        // instantiate a new scanner and use scanner to get player name and store in a string
+        // instantiate a new scanner and use scanner to get player name and store in a
+        // string
         Scanner scName = new Scanner(System.in);
         String name = scName.nextLine();
         System.out.println("Welcome " + name + ", can you guess how many sweets are in the jar?");
 
-        guessLoop(sweetCount, scName);
+        guessLoop(sweetCount, scName, numGuess);
     }
 
-    private static void guessLoop(int sweetCount, Scanner scName) {
+    private static void guessLoop(int sweetCount, Scanner scName, int numGuess) {
         for (int i = 0; i == 0;) {
 
             // get the players guess
             int pGuess = scName.nextInt();
 
             // if to check the users guess
-            if (pGuess == sweetCount ) {
-                System.out.println("Well done!!!");
+            if (pGuess == sweetCount) {
                 i = 1;
+                numGuess++;
+                System.out.println("Phil, You got it in '" + numGuess + "' Guesses");
+                System.out.println("Well Done!");
             }
-            if (pGuess < sweetCount ) {
+            if (pGuess < sweetCount) {
                 System.out.println("Nope! Try HIGHER");
+                numGuess++;
             }
-            if (pGuess > sweetCount ) {
+            if (pGuess > sweetCount) {
                 System.out.println("Nope! Try LOWER");
-
+                numGuess++;
             }
 
-            // improvements
-                // Add high score (maybe a writable file)
-                // Add a 'little' higher or lower prompt if ~50
+            // Improvements
+            // Added numberOfGusses 'numGuess' ✓
+            // Add high score (maybe a writable file)
+            // Add a 'little' higher or lower prompt if ~50
+            // Erorr Catching
 
         }
     }
